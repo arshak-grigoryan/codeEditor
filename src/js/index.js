15d1195e9fdeac73ebf160b2c2170e1b31f9ee50
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-28 00:20:23 +0400

    add js

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/index.css'
+import Model from './model/model'
+
+
+class View {
+    constructor () {
+
+    }
+    
+}
+
+
+const app = new View()
---
+++ src/js/model/folder.js
@@ -0,0 +1,33 @@
+export default class Folder {
+    constructor () {
+
+    }
+    
+}
+
+
+
+const projectEditor = { // ul
+    self: { // div
+        icon: 'icon', // i
+        name: 'projectEdiotr' // span
+    },
+    content: { // div
+        public: { // ul
+            self: { // div
+                icon: 'icon', // i
+                name: 'public' // span
+            },
+            content: {
+                index: {
+                    icon: 'icon', // i
+                    name: 'public' // span
+                }
+            }
+        },
+        src: { // ul
+
+        }
+
+    }
+}
