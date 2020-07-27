7f56281884f0b4aeb5d0da75e48ca58480c1bc3e
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-28 00:21:10 +0400

    create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = process.env.NODE_ENV === 'production'
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const optimization = () => {
+    const config = {
+
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCSSAssetsWebpackPlugin(),
+        ]
+    }
+    return config
+}
+
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: '../public/index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: filename('css'),
+        })
+    ]
+    return base
+}
+
+module.exports = {
+    context: path.resolve(__dirname,'src'),
+    mode: 'development',
+    entry: {
+        main:[
+            './js/index.js'
+        ]
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.css']
+    },
+    optimization: optimization(),
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCSSExtractPlugin.loader,'css-loader']
+            }
+        ]
+    },
+    devtool: isDev ? 'source-map' : '',
+    devServer: {
+        open: true
+    }
+}
