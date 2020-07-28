e5ee706ed3e9042be770bf24016337d5c2fa8fcc
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-29 00:19:29 +0400

    add folder

--- src/js/model/folder.js
+++ src/js/model/folder.js
@@ -1,10 +1,17 @@
 export default class Folder {
-    constructor () {
-
+    constructor ( {name, content = {} } ) {
+        this.self = {
+            name: name,
+            type: 'folder'
+        }
+        this.content = content
     }
     
 }
 
+const newFolder = new Folder( { name:"projectEditor" } )
+
+// console.log(newFolder)
 
 
 const projectEditor = { // ul
@@ -31,3 +38,92 @@ const projectEditor = { // ul
 
     }
 }
+
+
+// class Folder {
+//     constructor ( { name, type } ) {
+//         this.self: {
+//             name = name
+//             type = type            
+//         }
+//         this.content = {}
+//     }
+
+//     addFolder ( newFolder) {
+//         Object.defineProperty(this.content, newFolder.name, { value: newFolder})
+//         // this.content.push(newFolder)
+//     }
+
+//     addFile ( newFile ) {
+//         this.content.push(newFile)
+//     }
+
+//     get propName () {
+//         return this.name
+//     }
+
+//     set propName (newName) {
+//         this.name = newName
+//     }
+// }
+
+
+
+
+
+// class File {
+//     constructor ( { type, name, format } ) {
+//         this.type = type
+//         this.name = name
+//         this.format = format
+//         this.code = ''
+//     }
+
+//     get propName () {
+//         return this.name + '.' + this.format
+//     }
+
+//     set propName (newName) {
+//         this.name = newName
+//     }
+// }
+// const ROOT = new Folder( { name:'ROOT', type:'folder' } )
+
+
+// ROOT.addFolder(new Folder( { name:'firstProject', type:'folder' } ))
+
+
+// const proj1 = new Folder( { name:'proj1', type:'folder' } )
+// const index = new File( { type:'file', name:'index', format:'html' } )
+
+// Object.defineProperty(ROOT.content, 'index', { value: index })
+
+
+
+// const ROOT = {
+//     name: 'ROOT',
+//     type:'folder',
+
+//     firstProject: {
+//         name: 'firstProject',
+//         type:'folder',
+
+//         index:{type:'file', name:'index', format:'html'},
+//         src:{
+//             name: 'src',
+//             type:'folder',
+
+//             js:{
+//                 name: 'js',
+//                 type:'folder',
+    
+//                 controller: {type:'file', name:'controller', format:'js'},
+//                 model: {type:'file', name:'model', format:'js'},
+//                 view: {type:'file', name:'view', format:'js'}                
+//             },   
+//             css: {
+//                 style: {type:'file', name:'style', format:'css'}
+//             }               
+//         }
+//     }
+// }
