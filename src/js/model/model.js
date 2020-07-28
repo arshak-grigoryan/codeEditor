9e80f639726ae2d592d884c7b8a68197db2dfcf1
Anna6563 <anna.davtyan.6563@gmail.com>
2020-07-29 00:44:12 +0400

    add model, delete index

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -1,3 +1,22 @@
+import Folder from './folder'
+
 export default class Model {
+    constructor () {
+        this.project = null
+    }
+
+    init () {
+        this.firstTimeCreateFolder()
+    }
+
+    bindOnModelFirstTimeCreateFolder (cb) {
+        this.firstTimeCreateFolder = cb
+    }
     
+    addProject () {
+        console.log(555555555)
+
+        // const newProject = new Folder()
+        // this.project = newProject
+    }
 } 
