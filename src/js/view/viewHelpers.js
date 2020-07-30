bfb66bc19568b5b1ecbb368717ac96f51d73ec6c
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-30 12:50:27 +0400

    change classes to arr, add createFolder

--- src/js/view/viewHelpers.js
+++ src/js/view/viewHelpers.js
@@ -1,19 +1,32 @@
-function createEl({tag, parentEl, className, attributes, content}) {
+function createEl( { tag, parentEl, classes, attributes, content } ) {
   const el = document.createElement(tag)
-
-  if(className) {el.classList.add(className)}
+  if(classes) {
+    for (const val of classes) {
+      el.classList.add(val)
+    }
+  }
   if(attributes) {
-    for (const name in attributes) {  
+    for (const name in attributes) {
       el.setAttribute(name, attributes[name])
     }
   }
-  if(content) {el.textContent = content}
-  if(parentEl) {parentEl.appendChild(el)}
+  if(content) { el.textContent = content }
+  if(parentEl) { parentEl.appendChild(el) }
   return el
+}
+
+function creatFolder (folderName) {
+  const root = document.getElementById('root')
+  const ul = createEl( { tag:'ul', classes:['folder'], parentEl:root } )
+  const self = createEl( { tag:'div', classes:['self'], parentEl:ul } )
+  const i = createEl( { tag:'i', classes:['far', 'fa-folder'], parentEl:self } )
+  const span = createEl( { tag:'span', parentEl:self, content: folderName } )
+  const content = createEl( { tag:'div', classes:['content'], parentEl:ul } )
+  return ul
 }
 
 function createInput () {
 
 }
 
-export { createEl, createInput }
+export { createEl, creatFolder }
