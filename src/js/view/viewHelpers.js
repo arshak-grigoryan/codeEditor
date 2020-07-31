210f2ca5f83679d442d993d53360ef8f775f50c2
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-31 15:42:27 +0400

    add findParent, createFile, getIcon

--- src/js/view/viewHelpers.js
+++ src/js/view/viewHelpers.js
@@ -1,5 +1,30 @@
 import { ICONS } from '../../img/icons/icons'
 
+function getIcon (fileName) {
+  console.log('fileName', fileName)
+  console.log(fileName === /\.js$/)
+  if(/\.html$/.test(fileName)) {
+    const ext = fileName.slice(fileName.length-5, fileName.length)
+    if(ext) {
+      return ICONS.html5Icon
+    }
+  }
+  if(/\.css$/.test(fileName)) {
+    const ext = fileName.slice(fileName.length-4, fileName.length)
+    if(ext) {
+      return ICONS.css3Icon
+    }
+  }
+  if(/\.js$/.test(fileName)) {
+    const ext = fileName.slice(fileName.length-3, fileName.length)
+    if(ext) {
+      return ICONS.jsIcon
+    }
+  } else {
+    return ICONS.fileDefaultIcon
+  }
+}
+
 export function createEl( { tag, parentEl, classes, attributes, content } ) {
   const el = document.createElement(tag)
   if(classes) {
@@ -17,35 +42,36 @@ export function createEl( { tag, parentEl, classes, attributes, content } ) {
   return el
 }
 
-export function createIconsWrapper (isProjectNow) {
-  if( !isProjectNow ) {
-    const self = document.querySelector('#root > ul > .self')
-    self.style.position = 'relative'
-    const iconsWrapper = createEl( { tag:'div', classes:['iconsWrapper'], parentEl:self } )
-    const fileIcon = createEl( { tag:'img', classes:['fileIcon'], parentEl:iconsWrapper, attributes:{ 'src':ICONS.fileIconLightIcon } } )
-    const folderIcon = createEl( { tag:'img', classes:['folderIcon'], parentEl:iconsWrapper, attributes:{ 'src':ICONS.folderIconLight } } )
-    return iconsWrapper
-  }  
+export function createIconsWrapper () {
+  const self = document.querySelector('#root > ul > .self')
+  const iconsWrapper = createEl( { tag:'div', classes:['iconsWrapper'], parentEl:self } )
+  const fileIcon = createEl( { tag:'img', classes:['fileIcon'], parentEl:iconsWrapper, attributes:{ 'src':ICONS.fileIconLightIcon } } )
+  const folderIcon = createEl( { tag:'img', classes:['folderIcon'], parentEl:iconsWrapper, attributes:{ 'src':ICONS.folderIconLight } } )
+  return iconsWrapper
 }
 
-export function creatFolder ( [ name ] ) {
+export function creatFolder ( [ name, parentElem ] ) {
   const root = document.getElementById('root')
-  const ul = createEl( { tag:'ul', classes:['folder'], parentEl:root } )
-  const self = createEl( { tag:'div', classes:['self'], parentEl:ul } )
+  const ul = createEl( { tag:'ul', classes:['folder'], parentEl:parentElem || root, attributes: { parentId: addId() } } )
+  const self = createEl( { tag:'div', classes:['self'], parentEl:ul, attributes: { tabindex:'0' } } )
   const iconArrowWrapper = createEl( { tag:'div', classes:['iconArrowWrapper'], parentEl:self } )
   const iconArrow = createEl( { tag:'img', classes:['iconArrow'], parentEl:iconArrowWrapper, attributes:{ 'src': ICONS.arrowRightIcon} } )
   const iconimg = createEl( { tag:'img', parentEl:self, attributes: { src: ICONS.folderIcon } } )
-  // const i = createEl( { tag:'i', classes:['far', 'fa-folder'], parentEl:self } )
   const span = createEl( { tag:'span', parentEl:self, content: name } )
   const content = createEl( { tag:'div', classes:['content'], parentEl:ul } )
   return ul
 }
 
-export function createInput () {
+export function createFile ( [ name, parentElem ] ) {
+  const li = createEl( { tag:'li', classes:['file'], parentEl:parentElem, attributes: { tabindex:'0' } } )
+  const iconimg = createEl( { tag:'img', parentEl:li, attributes: { src: getIcon(name)} } )
+  const span = createEl( { tag:'span', parentEl:li, content: name } )
+  return li
+}
+
+export function createInput ( parentElement ) {
   const root = document.getElementById('root')
-  const createFolderWrapper = document.querySelector('.createFolderWrapper')
-  createFolderWrapper.remove()
-  const inputNameDiv = createEl( { tag:"div", parentEl:root, classes:['inputNameDiv'] } )
+  const inputNameDiv = createEl( { tag:"div", parentEl: parentElement || root, classes:['inputNameDiv'] } )
   const form = createEl( { tag:'form', parentEl:inputNameDiv } )
   const inputName = createEl( { tag:'input', parentEl:form, classes: ['inputNameInput'], attributes: { 'autofocus': true } } )
   return inputName
@@ -53,9 +79,27 @@ export function createInput () {
 
 
 const IDs = [0];
-const maxId = Math.max.apply(null, IDs)
+let maxId = Math.max.apply(null, IDs)
 export function addId () {
   maxId += 1;
   IDs.push(maxId)
   return maxId;
 }
+
+// export function nameChecker (name) {
+
+// }
+
+export function findParent (el) {
+  const attrClass = el.getAttribute('class')
+  if (attrClass === 'folder' || attrClass === 'file') {
+    console.log('000', el, el.children[0].children[2])
+    return { currentThis:el, className:attrClass, name:el.children[0].children[2].textContent }
+  }
+  if(attrClass === 'fileIcon' ||  attrClass === 'folderIcon') {
+    return { currentThis:el.parentElement.parentElement.parentElement.children[1], className:attrClass }
+  } else {
+    return findParent (el.parentElement)
+  }
+}
+
